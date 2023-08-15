7089f4c60f2fa88b9e434a5d770759130c577a2c
MatheoSteinbach <matheosteinbach99@gmail.com>
2023-08-15 17:04:08 +0200

    Portfolio ver. 1
    Added Home, Projects, CV pages

--- assets/js/research.js
+++ assets/js/research.js
@@ -4,17 +4,22 @@ const researchTable = document.querySelector(".main");
 
 const research = [
   {
-    title: "A Batch Normalized Inference Network Keeps the KL Vanishing Away",
-    authors:
-      "Qile Zhu, Wei Bi, Xiaojiang Liu, Xiyao Ma, Xiaolin Li and Dapeng Wu",
-    conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
+    title: "Tanuki's Dream",
+	ref: "https://s4g.itch.io/tanukis-dream",
+    subTitel:
+      "S4G School for Games GmbH, 3rd Semester Project",
+    description:
+      "Tanuki's Dream is a 2D top-down, action-adventure game. Defeat enemies to appropiate their abilities and use them to your advantage! Fight your way through the kappa-infested swamp and find the hidden Tanuki village. Made with the Unity Engine!",
+    subTitel2:
+	"My Responsibilities",
+	responsibilities:
+	"Leading a Team of 5 during 10 weeks, I was responsible for the Programming, Game Design and Production. Some key tasks I did contain the making of AI for the Enemies, an immersive interaction system, intricate combat mechanics and more",
+	researchYr: 2023,
     citebox: "popup1",
-    image: "assets/images/research-page/inteferenceNetwork.png",
+    image: "assets/images/project-page/tanukisdream.png",
     citation: {
       vancouver:
-        "Qile Zhu, Wei Bi, Xiaojiang Liu, Xiyao Ma, Xiaolin Li and Dapeng Wu. A Batch Normalized Inference Network Keeps the KL Vanishing Away. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
+        "School 4 Games.",
     },
     abstract:
       "This is currently left empty and this can be considered as a dummy data 1",
@@ -22,14 +27,19 @@ const research = [
   },
 
   {
-    title: "A Call for More Rigor in Unsupervised Cross-lingual Learning",
-    authors:
-      "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre",
-    conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
+    title: "Convoy's Hope",
+	ref: "https://s4g.itch.io/convoys-hope",
+    subTitel:
+      "S4G School for Games GmbH, 4th Semester Project",
+    description:
+      "coming soon",
+    subTitel2:
+	"My Responsibilities",
+	responsibilities:
+	"coming soon",
+	researchYr: 2023,
     citebox: "popup2",
-    image: "assets/images/research-page/crossLingual.png",
+    image: "assets/images/project-page/convoyshope.png",
     citation: {
       vancouver:
         "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre. A Call for More Rigor in Unsupervised Cross-lingual Learning. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
@@ -41,16 +51,21 @@ const research = [
 
   {
     title:
-      "A Comprehensive Analysis of Preprocessing for Word Representation Learning in Affective Tasks",
-    authors: "Nastaran Babanejad, Ameeta Agrawal, Aijun An and Manos Papagelis",
-    conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
+      "Urban Outcasts",
+	ref: "https://s4g.itch.io/urban-outcasts",
+    subTitel: "S4G School for Games GmbH, 2nd Semester Project",
+    description:
+      "Urban Outcasts is an isometric 2.5D Adventure with RPG elements. The player’s goal is to explore the setting of the story through a variety of characters by talking and completing quests. The world is filled with diverse characters from different worlds, cultures and creature races. Made with the Unity Engine!",
+    subTitel2:
+	"My Responsibilities",
+	responsibilities:
+	"My responsibilities during the 10 weeks were mainly programming. Some key tasks I did were the Movement in this 2.5D World, AI for the NPC's, a Interaction System, Quest System and more",
+	researchYr: 2022,
     citebox: "popup3",
-    image: "assets/images/research-page/wordRepresentation.png",
+    image: "assets/images/project-page/urbanoutcasts.png",
     citation: {
       vancouver:
-        "Nastaran Babanejad, Ameeta Agrawal, Aijun An and Manos Papagelis. A Comprehensive Analysis of Preprocessing for Word Representation Learning in Affective Tasks. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
+        "Npreh",
     },
     abstract:
       "This is currently left empty and this can be considered as a dummy data 3",
@@ -59,14 +74,19 @@ const research = [
 
   {
     title:
-      "A Contextual Hierarchical Attention Network with Adaptive Objective for Dialogue State Tracking",
-    authors:
-      "Yong Shan, Zekang Li, Jinchao Zhang, Fandong Meng, Yang Feng, Cheng Niu and Jie Zhou",
-    conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
+      "Presidential Driver",
+	ref: "https://s4g.itch.io/presidential-driver",
+    subTitel:
+      "S4G School for Games GmbH, 1st Semester Project",
+    description:
+      "Presidential Driver is a 2D-Action-Platformer in a sci-fi setting. With the help of Wall jumps, the goal is to reach the top as fast as possible! Made with the Godot Engine!",
+    subTitel2:
+	"My Responsibilities",
+	responsibilities:
+	"In a Team of 7, as the only programmer I was responsible for everything in-engine and code related. Some Key tasks were making sure the movement is smooth, Enemy AI, Save and Load System",
+	researchYr: 2022,
     citebox: "popup4",
-    image: "assets/images/research-page/dialogueState.png",
+    image: "assets/images/project-page/presidentialdriver.png",
     citation: {
       vancouver:
         "Yong Shan, Zekang Li, Jinchao Zhang, Fandong Meng, Yang Feng, Cheng Niu and Jie Zhou. A Contextual Hierarchical Attention Network with Adaptive Objective for Dialogue State Tracking. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
@@ -74,77 +94,7 @@ const research = [
     abstract:
       "This is currently left empty and this can be considered as a dummy data 4",
     absbox: "absPopup4",
-  },
-
-  {
-    title: "Dual Super-Resolution Learning for Semantic Segmentation",
-    authors: "Wang, Li and Li, Dong and Zhu, Yousong and Tian, Lu and Shan, Yi",
-    conferences:
-      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
-    researchYr: 2020,
-    citebox: "popup5",
-    image: "assets/images/research-page/semanticSegmentation.png",
-    citation: {
-      vancouver:
-        "Wang, Li and Li, Dong and Zhu, Yousong and Tian, Lu and Shan, Yi. Dual Super-Resolution Learning for Semantic Segmentation. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 5",
-    absbox: "absPopup5",
-  },
-
-  {
-    title: "Deep Unfolding Network for Image Super-Resolution",
-    authors: "Zhang, Kai and Van Gool, Luc and Timofte, Radu",
-    conferences:
-      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
-    researchYr: 2020,
-    citebox: "popup6",
-    image: "assets/images/research-page/deepNetwork.png",
-    citation: {
-      vancouver:
-        "Zhang, Kai and Van Gool, Luc and Timofte, Radu. Deep Unfolding Network for Image Super-Resolution. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 6",
-    absbox: "absPopup6",
-  },
-
-  {
-    title:
-      "Unsupervised Learning for Intrinsic Image Decomposition From a Single Image",
-    authors: "Liu, Yunfei and Li, Yu and You, Shaodi and Lu, Feng",
-    conferences:
-      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
-    researchYr: 2020,
-    citebox: "popup7",
-    image: "assets/images/research-page/imageDecomposition.png",
-    citation: {
-      vancouver:
-        "Liu, Yunfei and Li, Yu and You, Shaodi and Lu, Feng. Unsupervised Learning for Intrinsic Image Decomposition From a Single Image. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 7",
-    absbox: "absPopup7",
-  },
-  {
-    title:
-      "Forward and Backward Information Retention for Accurate Binary Neural Networks",
-    authors:
-      "Qin, Haotong and Gong, Ruihao and Liu, Xianglong and Shen, Mingzhu and Wei, Ziran and Yu, Fengwei and Song, Jingkuan",
-    conferences:
-      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
-    researchYr: 2020,
-    citebox: "popup8",
-    image: "assets/images/research-page/neuralNetworks.jpg",
-    citation: {
-      vancouver:
-        "Qin, Haotong and Gong, Ruihao and Liu, Xianglong and Shen, Mingzhu and Wei, Ziran and Yu, Fengwei and Song, Jingkuan. Forward and Backward Information Retention for Accurate Binary Neural Networks. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 8",
-    absbox: "absPopup8",
-  },
+  }
 ];
 AOS.init();
 const fillData = () => {
@@ -153,8 +103,11 @@ const fillData = () => {
     ({
       image,
       title,
-      authors,
-      conferences,
+	  ref,
+      subTitel,
+	  subTitel2,
+	  responsibilities,
+      description,
       researchYr,
       citebox,
       citation,
@@ -170,33 +123,21 @@ const fillData = () => {
                             <img src="${image}" class="imgRes">
                         </span>
                     </div>
-                    <a href="#0" class="paperTitle"> ${title} </a> 
-                    <div class = "authors"> ${authors} </div> 
+                    <a href="${ref}" target="_blank" class="paperTitle"> ${title} </a> 
+                    <div class = "subTitel"> ${subTitel} </div> 
                     
-                    <div class="rConferences"> ${conferences} 
-                        <div class="researchY">${researchYr}</div>
-                    </div>
+                    <div class="rdescription"> ${description} 
+                    <div class="researchY">${researchYr}</div>
+					</div>
+					
+					&nbsp
+					<div class = "subTitel"> ${subTitel2} </div> 
                     
-                    <!--CITE BUTTON-->
-                    <div class="d-flex" style="margin-right:5%;">
-                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${absbox}" aria-expanded="false" aria-controls="${absbox}">
-                            ABSTRACT
-                        </button>
-                
-                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${citebox}" aria-expanded="false" aria-controls="${citebox}">
-                            CITE
-                        </button>
+                    <div class="rdescription"> ${responsibilities} 
                     </div>
-                    <div id="${absbox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
-                        <div class="card-body">
-                            ${abstract}    
-                        </div>
-                    </div>
-                    <div id="${citebox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
-                        <div class="card-body">
-                            ${citation.vancouver}    
-                        </div>
-                    </div>
+					
+                    
+                    
                 </td>
             </tr>`)
   );
